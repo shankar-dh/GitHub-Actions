746b7c29333f00a6a48aa7fefe96ff845209815b
Dhanush Kumar <shankar.dh@northeastern.edu>
2023-09-08 04:19:20 -0400

    added docstrings

--- src/calculator.py
+++ src/calculator.py
@@ -1,13 +1,48 @@
 def fun1 (x, y):
+    """
+    Adds two numbers together.
+    Args:
+        x (int/float): First number.
+        y (int/float): Second number.
+    Returns:
+        int/float: Sum of x and y.
+    """
     return x + y
 
 def fun2 (x, y):
+    """
+    Subtracts two numbers.
+    Args:
+        x (int/float): First number.
+        y (int/float): Second number.
+    Returns:
+        int/float: Difference of x and y.
+    """
     return x - y
 
 def fun3 (x, y):
+    """
+    Multiplies two numbers together.
+    Args:
+        x (int/float): First number.
+        y (int/float): Second number.
+    Returns:
+        int/float: Product of x and y.
+    """
     return x * y
 
 def fun4(x,y):
+    """
+    Adds the results of fun1, fun2, and fun3 together.
+    Args:
+        x (int/float): First number.
+        y (int/float): Second number.
+    Returns:
+        int/float: Sum of fun1, fun2, and fun3.
+        Raises:
+            ValueError: If either x or y is not a number.
+    
+    """
     if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
         raise ValueError("Both inputs must be numbers.")
     f1_result = fun1(x,y)
