e3aadb3792251c0e388bbc0cd167223ee05f4fa4
Dhanush Kumar <shankar.dh@northeastern.edu>
2023-09-07 21:04:10 -0400

    updated readme and modified file structure

---
+++ src/calculator.py
@@ -0,0 +1,16 @@
+def fun1 (x, y):
+    return x + y
+
+def fun2 (x, y):
+    return x - y
+
+def fun3 (x, y):
+    return x * y
+
+def fun4(x,y):
+    f1_result = fun1(x,y)
+    f2_result = fun2(x,y)
+    f3_result = fun3(x,y)
+    total = f1_result + f2_result + f3_result
+    return total
+
